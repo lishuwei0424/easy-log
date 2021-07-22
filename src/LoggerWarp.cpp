85cdc650582b7c2e964bdd01777323e5a4915fd9
李书伟 <2022853403@qq.com>
2021-07-22 14:06:54 -0400

    add module log

--- src/LoggerWarp.cpp
+++ src/LoggerWarp.cpp
@@ -55,3 +55,34 @@ void LoggerWarp::Print( const char* file, long line, const char* funtion, LOG_LE
          break;
     };
 }
+
+void LoggerWarp::PrintWithModule( const char* file, long line, const char* funtion, LOG_LEVEL level, const char* module,const char* format, ... )
+{   
+	char buff[4096];
+	va_list ap;
+	va_start(ap, format);
+	vsnprintf(buff, 4096, format, ap);
+	va_end(ap);
+ 
+	char strlog[4096]={0};
+    snprintf(strlog, 4096, "[%s][%s][%s][%d]:%s", module,file,funtion,line,buff);
+    
+	switch(level)
+    {
+    case LoggerWarp::ERROR:
+         LOG4CXX_ERROR(rootLogger,strlog);
+        break;
+    case LoggerWarp::INFO:
+         LOG4CXX_INFO(rootLogger,strlog);
+        break;
+    case LoggerWarp::DEBUG:
+         LOG4CXX_DEBUG(rootLogger,strlog);
+        break;
+    case LoggerWarp::WARN:
+         LOG4CXX_WARN(rootLogger,strlog);
+        break;
+    default:
+         LOG4CXX_TRACE(rootLogger,strlog);
+         break;
+    };
+}
