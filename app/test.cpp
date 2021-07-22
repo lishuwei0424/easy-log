16f236bd84a3757cfa8f6d78a6b95f7ef5983ca0
李书伟 <2022853403@qq.com>
2021-07-22 14:07:44 -0400

    Update test.cpp

--- app/test.cpp
+++ app/test.cpp
@@ -6,11 +6,11 @@ int main()
 { 
   while (1)
   {
-      LOG_ERROR("%s","lsw");
+      LOG_ERROR("%s,%d","lsw",100000);
       LOG_INFO("%s","HAHHAHFHAH");
       LOG_DEBUG("%d",10);
       LOG_DEBUG("%s","WARN");
-
+      TASK_ERROR("%s","FUNC ERROR");
       sleep(2);
   }
    return  0;
