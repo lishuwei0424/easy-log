b7d171443245a72bb7b1ea7537473ccd4ab3b61b
lishuwei0424 <2022853403@qq.com>
2021-07-22 14:01:12 -0400

    add model log

---
+++ app/CMakeFiles/esay-log.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esay-log.dir/src/LoggerWarp.cpp.o"
+  "CMakeFiles/esay-log.dir/test.cpp.o"
+  "esay-log.pdb"
+  "esay-log"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esay-log.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
